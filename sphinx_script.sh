d2de1d15ce16f5cb2200120c15da817d510ed3ed
zenenhornstein <zenenhornstein2@gmail.com>
2020-04-10 19:27:35 -0700

    sphinx script to do nearly everything to avoid a headache

---
+++ sphinx_script.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+mkdir docs
+cd docs
+sphinx-quickstart
+
+#Change conf.py
+sed -i '14,17s/#\s//' conf.py
+#Do path
+sed "17s/abspath('.')/abspath('..\/')/" conf.py
+#Change index.rst
+sed '13a\   modules' index.rst
+
+sphinx-apidoc -o . ..
+sleep 1
+make html
+sleep 1
+google-chrome http://localhost:8000/ && python3 -m http.server -d _build/html
+
+
